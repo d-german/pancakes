08e573f0116b72a9f6fa24129ce8eb6e69f27bb9
dgerman <damon.german@perceptivesoftware.com>
2015-12-04 20:15:52 -0600

    Initial commit

---
+++ PancakesWPF/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+using PancakesWPF.ViewModel;
+
+namespace PancakesWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        /// <summary>
+        /// Initializes a new instance of the MainWindow class.
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+            Closing += (s, e) => ViewModelLocator.Cleanup();
+        }
+    }
+}
